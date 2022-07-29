888f71593ad97ff4829398b7bd221870361e3877
Maks Kostyshen <maxxxymko2003@gmail.com>
2022-07-29 23:58:58 +0300

    start changing encoder: makes cyrillic symbols latin

--- src/main/java/strategies/fieldValueGetters/Encoder.java
+++ src/main/java/strategies/fieldValueGetters/Encoder.java
@@ -2,15 +2,36 @@ package strategies.fieldValueGetters;
 
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Encoder {
 
     public Encoder(){}
 
-    public String encode(String stringToEncode) throws UnsupportedEncodingException {
-        byte[] stringBytes = stringToEncode.getBytes("CP1251");
+    public String encode(String stringToEncode){
+        StringBuilder builder = new StringBuilder();
 
-        return new String(stringBytes, Charset.forName("CP1251"));
+        for(char c : stringToEncode.toCharArray()){
+            String newValue = encodeMap.get(c);
+            if(newValue != null){
+                builder.append(c);
+            }
+        }
+        return builder.toString();
     }
 
+
+
+    private static Map<Character, String> encodeMap = new HashMap<Character, String>(){{
+       put('а', "a");
+       put('б', "b");
+    }};
+
 }
+
+
+
+
+
+
